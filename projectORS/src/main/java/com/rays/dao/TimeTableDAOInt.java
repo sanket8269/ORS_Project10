eca357d22bcd5a34c7472753778b52de354a35a8
SANKET JAIN <99023318+sanket8269@users.noreply.github.com>
2023-04-12 14:06:44 +0530

    New

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Sanket Jain
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
